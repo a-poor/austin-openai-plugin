f0760b18c7be166731f8e835e7ae6fdc69916c59
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-09-24 11:58:08 -0700

    Initializes hono API w/ OpenAI plugin config

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { Hono } from 'https://deno.land/x/hono@v3.7.2/mod.ts';
+import { logger } from 'https://deno.land/x/hono@v3.7.2/middleware.ts';
+import { cors } from 'https://deno.land/x/hono@v3.7.2/middleware.ts'
+import { serveStatic } from 'https://deno.land/x/hono@v3.7.2/middleware.ts';
+
+
+function createApp() {
+  const app = new Hono();
+  app.use("*", logger());
+  app.use("*", cors({
+    origin: ["http://localhost:8000", "https://chat.openai.com"],
+  }));
+  app.get('/', c => c.html("<h1>Hello World</h1>"));
+  app.get('/todos', c => c.json({ 
+    todos: [
+      "Get milk",
+      "Get eggs",
+      "Make cookies",
+    ],
+  }));
+  app.use('/*', serveStatic({ root: './static' }))
+  return app;
 }
+
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const app = createApp();
+  Deno.serve(app.fetch);
 }
